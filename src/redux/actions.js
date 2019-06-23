260d6017b230763be7c382ea006d6e7c5e7b076c
Christopher Pardy <chris@rategravity.com>
2019-06-22 22:18:36 -0400

    Add GetRates Thunk
    Thunk dispatches standard action and kicks off API calls.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,35 @@
 
+const API = "https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes"
+
+export const getRatesThunk = ({ loanSize, creditScore, propertyType, occupancy }) => (
+  dispatch => {
+    dispatch(getRates({ loanSize, creditScore, propertyType, occupancy }));
+    fetch(
+      `${API}?loanSize=${loanSize}&creditScore=${creditScore}&propertyType=${propertyType}&occupancy=${occupancy}`,
+      {
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': 'RG-AUTH 71d50f56-6377-4196-9823-6f61b512899c'
+        })
+      }
+    ).then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('Sorry, something went wrong. Please try again. ')
+      }
+    })
+    .then(
+      response => {
+        console.log(`got resposne ${JSON.stringify(response.rateQuotes)}`);
+      }
+    )
+    .catch(error => {
+      console.error(error);
+    })
+  }
+)
+
 export const GetRatesType = 'GetRates';
 export const getRates = ({ loanSize, creditScore, propertyType, occupancy }) => ({
   type: GetRatesType,
